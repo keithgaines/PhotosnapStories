25c38fd027ebb0538797294d1fb73131857e0236
Keith Gaines <93451842+keithgaines@users.noreply.github.com>
2026-05-17 17:24:44 -0500

    fix: bundle global styles through Vite entrypoint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,12 @@
 // web/src/App.tsx
 import React from 'react'
-
+import './index.css'
+import './navbar.css'
+import './features.css'
+import './imagecard.css'
+import './pricing.css'
+import './stories.css'
+import './footer.css'
 import { RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
