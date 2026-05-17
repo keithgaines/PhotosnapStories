478db47ef8908aa7eed888659691dcc04ff2d8e9
Keith Gaines <93451842+keithgaines@users.noreply.github.com>
2026-05-17 18:28:10 -0500

    feat: complete responsive frontend modernization

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 import { RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
 import Routes from 'src/Routes'
 
 const App = () => (
